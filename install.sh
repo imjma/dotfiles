ef21c8e8dfafdc23c2bda35ec4820fa280d79dee
Jie Ma <codejma@gmail.com>
2014-04-28 20:28:43 +1000

    add gitignore

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 dir=$HOME/dotfiles                    # dotfiles directory
-files="bashrc bash_profile zshrc gitconfig"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile zshrc gitconfig vimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -16,7 +16,7 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file $HOME/.$file
