6f77a1873609d47a5cb78229cc1606626f529344
Jie Ma <codejma@gmail.com>
2015-10-09 11:40:19 +1100

    add command -v

--- zsh/fasd.zsh
+++ zsh/fasd.zsh
@@ -1,11 +1,12 @@
 #
 # only init if installed.
-fasd_cache="$HOME/.fasd-init-bash"
-if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-  eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
-fi
-source "$fasd_cache"
-unset fasd_cache
+if [[ "$(command -v fasd)" > /dev/null ]]; then
+  fasd_cache="$HOME/.fasd-init-bash"
+  if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+    eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
+  fi
+  source "$fasd_cache"
+  unset fasd_cache
 
 
 # jump to recently used items
@@ -16,4 +17,5 @@ unset fasd_cache
 # alias z='fasd_cd -d' # cd, same functionality as j in autojump
 # alias zz='fasd_cd -d -i' # interactive directory jump
 
-alias v='f -e vim' # quick opening files with vim
+  alias v='f -e vim' # quick opening files with vim
+fi
