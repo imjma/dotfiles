fe3a772b940cb172864f128a35ef91febbd8f92d
Jie Ma <codejma@gmail.com>
2017-09-01 19:30:10 +1000

    awesome aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,10 +8,40 @@ if [[ `command -v nvim` > /dev/null ]]; then
   # alias vim="nvim"
   alias v="nvim"
 fi
+# git
+alias nah="git reset --hard;git clean -df"
+alias wip="git add . && git commit -m 'wip'"
 
 # copy directory
 alias cpd="cp -avR"
+alias copy="rsync -avv --stats --human-readable --itemize-changes --progress --partial"
 
-alias fs="foreman start -f"
+# vagrant
 alias vup="vagrant up"
 alias vdown="vagrant halt"
+alias vssh="vagrant ssh"
+alias vgo="vagrant up & vagrant ssh"
+
+# php
+alias p="vendor/bin/phpunit"
+alias pf="vendor/bin/phpunit --filter"
+
+# composer
+alias c="composer"
+alias cu='composer update'
+alias ci='composer install'
+alias cda='composer dump-autoload -o'
+
+# laravel
+alias art="php artisan"
+alias tinker="php artisan tinker"
+alias laravel-create="composer create-project --prefer-dist laravel/laravel"
+
+# osx
+alias finder="open -a 'Finder' ."
+
+# functionns
+mkcdir () {
+    mkdir -p -- "$1" &&
+    cd -P -- "$1"
+}
