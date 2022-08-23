681d4ef9d536367926757b8a88093925e62d3f7a
Jie Ma <ma.jie@abc.net.au>
2022-08-23 11:00:42 +1000

    Add tab to cmp

--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -46,8 +46,21 @@ local lsp_flags = {
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require'lspconfig'.gopls.setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-    capabilities = capabilities
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  init_options = {
+    usePlaceholders = true,
+  }
 }
 
