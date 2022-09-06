23dafb3a6c74d8340905a3478e0d8c40db70654c
Jie Ma <ma.jie@abc.net.au>
2022-09-06 11:44:02 +1000

    Add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'PragmataPro',
+  -- font = wezterm.font 'MPLUSCodeLatin60 Nerd Font Mono',
+  font_size = 12,
+  line_height = 1.1,
+  window_background_opacity = 0.9,
+  enable_tab_bar = false,
+  color_scheme = "Gruvbox Dark",
+}
