bbedd99985207c0eecd04777df0cd4c0f877cf22
Jie Ma <im@jma.dev>
2022-09-07 17:08:02 +1000

    Update opacity

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
   font = wezterm.font 'MPLUSCodeLatin60 Nerd Font Mono',
   font_size = 12,
   line_height = 1.1,
-  window_background_opacity = 0.9,
+  window_background_opacity = 0.95,
   enable_tab_bar = false,
   color_scheme = "Gruvbox Dark",
 }
